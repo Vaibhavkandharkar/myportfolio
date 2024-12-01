c02a3205752f3bb526815b3ec6c312746c90caa0
vaibhav kandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-12-01 17:04:18 +0000

    This is a changes for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import React from "react";
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Technologies from './components/Technologies'
 import Projects from './components/Projects'
 import Experience from './components/Experience'
-import Contact from './components/contact'
+import Contact from './components/Contact';  // Correct path
+
 function App() {
   
   return (
