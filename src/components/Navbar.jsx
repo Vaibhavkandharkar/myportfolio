40bb2dc160a82173a9a906808e08ce0cb7ffa6ae
vaibhav kandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-12-07 14:03:35 +0000

    Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <nav className='flex items-ceter justify-between'>
         <div className='flex flex-shrink-0 items-center'>
             <a href="/" aria-label='Home'>
-            <img src={logo} className='mx-1' width={130} height={60} alt="Logo"/>
+            <img src={logo} className='mx-1 my-6' width={100} height={40} alt="Logo"/>
             </a>
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
