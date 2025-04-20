a03dfb3e6f76f169c60777efd3fe8e1c0568840e
Vaibhav S Kandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2025-04-20 11:29:17 +0000

    small changes

--- src/extra/index.js
+++ src/extra/index.js
@@ -2,16 +2,12 @@ import project1 from "../assets/projects/gmail.webp";
 import project2 from "../assets/projects/img1.webp";
 import project3 from "../assets/projects/project-3.webp";
 import project5 from "../assets/projects/img2.webp"
-import project4 from "../assets/projects/project-4.webp";
+// import project4 from "../assets/projects/project-4.webp";
 import project6 from "../assets/projects/img3.webp";
 import project7 from "../assets/projects/card.webp"
 
 
 export const HERO_CONTENT = `As a passionate Full Stack Developer and a B.Tech student in Computer Science and Engineering at CSMSS Chh. Shahu College of Engineering, I thrive at the intersection of innovation and practicality. My role at PayPerJump Pvt. Ltd. has enabled me to merge academic knowledge with real-world challenges, delivering impactful web solutions. Over the past Ten months, I have honed my skills in front-end and full-stack development, crafting dynamic websites from scratch and utilizing technologies like ReactJS, Javascript, MongoDB, Expressjs, nodejs, HTML, CSS, Bootstrap, and PHP.`;
-
-// I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.
-
-// export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
@@ -22,13 +18,7 @@ export const EXPERIENCES = [
     technologies: ["HTML", "CSS", "Javascript","PHP","React.js","Bootstrap","SQL Database"],
   },
   
-  // {
-  //   year: "2020 - 2021",
-  //   role: "Software Engineer",
-  //   company: "Paypal",
-  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  // },
+  
 ];
 
 export const PROJECTS = [
