52be32c515658ab05778bf4cee79d26bf98adb1b
Vaibhav S Kandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2025-12-11 14:25:45 +0000

    make in change in resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -63,7 +63,7 @@ const Hero = () => {
             </motion.p>
             <motion.a
               variants={childVariants}
-              href="/VaibhavKandharkar_Resume_Java_pdf (3).pdf"
+              href="/Vaibhav-Kandharkar-Resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="bg-white rounded-full p-4  text-stone-800 mb-10"
